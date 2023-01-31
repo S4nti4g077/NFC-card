d7c2ab360ed9c967264c510c3b48ea85b4d80315
Your Name <santiago.perez777@gmail.com>
2023-01-31 14:01:00 +0100

    changed text

--- app.js
+++ app.js
@@ -231,3 +231,31 @@ document.getElementById("form").addEventListener("submit", function (event) {
 });
 
 // emailjs.com
+
+// accordion css
+
+const carerForm = document.getElementById("carerForm");
+console.log("carerForm", carerForm);
+
+carerForm.addEventListener("submit", function (e) {
+  alert();
+  e.preventDefault();
+
+  var action = this.attr("action");
+  console.log("action", action);
+
+  $.ajax({
+    url: "https://fiddle.jshell.net/favicon.png",
+    data: {
+      name: formElements["name"].value,
+      surname: formElementsT["surname"].value,
+      mail: formElementsT["mail"].value,
+      phone: formElements["phone"].value,
+      vacancy: formElements["vacancy"].value,
+      file: formElements["file"].value,
+    },
+    success: function (response) {
+      console.log(response);
+    },
+  });
+});
